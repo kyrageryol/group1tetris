9464f25278f003c604cf7c45d6dadcdb269a815b
kelleymerrell <kmerrell@luc.edu>
2021-04-24 13:58:03 -0500

    Updated sounds

--- js/main.js
+++ js/main.js
@@ -439,3 +439,17 @@ function drawBlock(ctx, x, y, color) {
 //-------------------------------------------------------------------------
 
 run();
+
+const aud = document.getElementById("playMusic").children[0];
+var isPlaying = false;
+aud.pause();
+
+function playPause()
+{
+  if (isPlaying){
+    aud.pause();
+  } else{
+    aud.play()
+  }
+  isPlaying = !isPlaying;
+}
